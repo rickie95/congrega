a2065cda4d3be8c64c2b1cd07f233c6152ecc43c
Riccardo Malavolti <riccardo.malavolti@gmail.com>
2021-04-06 19:10:08 +0200

    Home page now greets with name, is not available uses username

--- lib/features/dashboard/presentation/HomePage.dart
+++ lib/features/dashboard/presentation/HomePage.dart
@@ -1,7 +1,10 @@
 import 'package:congrega/features/lifecounter/presentation/LifeCounterPage.dart';
 import 'package:congrega/features/drawer/CongregaDrawer.dart';
+import 'package:congrega/features/loginSignup/model/User.dart';
+import 'package:congrega/features/users/UserRepository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:kiwi/kiwi.dart';
 
 import 'widgets/DashboardTinyTile.dart';
 import 'widgets/EventsWidget.dart';
@@ -25,11 +28,20 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(title: const Text(pageTitle)),
       drawer: CongregaDrawer(),
       body: ListView(
-        padding: EdgeInsets.symmetric(horizontal: 10),
+          padding: EdgeInsets.symmetric(horizontal: 10),
           children: [
 
             Container(
-              child: Text(AppLocalizations.of(context)!.dashboard_message + " " + getUsername(), style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              child: FutureBuilder(
+                future: getUsername(),
+                builder: (context, AsyncSnapshot<String> snapshot) {
+                  if(snapshot.connectionState == ConnectionState.waiting)
+                    return const Text('');
+                  if(snapshot.hasData && snapshot.data != null)
+                    return Text(AppLocalizations.of(context)!.dashboard_message + " " + snapshot.data!, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold));
+                  return const Text('');
+                },
+              ),
               padding: EdgeInsets.symmetric(vertical: 10.0),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(12),
@@ -44,14 +56,14 @@ class HomePage extends StatelessWidget {
               padding: const EdgeInsets.only(bottom: 24),
               child: Row(
                 children: [
-                  
+
                   Expanded(
                     flex: 50,
                     child: DashboardTinyTile(AppLocalizations.of(context)!.quick_match,
                         AppLocalizations.of(context)!.quick_match_subtitle,
                         Icons.favorite, Colors.redAccent, () => Navigator.of(context).push(LifeCounterPage.route())),
                   ),
-                  
+
                   Expanded(
                     flex: 50,
                     child: DashboardTinyTile(AppLocalizations.of(context)!.my_profile_title,
@@ -75,9 +87,9 @@ class HomePage extends StatelessWidget {
     //context.read<AuthenticationBloc>().add(AuthenticationLogoutRequested());
   }
 
-  String getUsername(){
-    // returns either the user's name or his username
-    return "John";
+  Future<String> getUsername() async {
+    return KiwiContainer().resolve<UserRepository>().getUser()
+        .then((User loggedUser) => loggedUser.name.isNotEmpty ? loggedUser.name : loggedUser.username);
   }
 
 }
