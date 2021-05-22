c39fd2e0420a789da22764445e76a906dc3f809c
Riccardo Malavolti <riccardo.malavolti@gmail.com>
2021-05-22 16:50:04 +0200

    adds uri for utility class Arcano

--- lib/utils/Arcano.dart
+++ lib/utils/Arcano.dart
@@ -1,3 +1,8 @@
+import 'dart:io';
+
+import 'package:graphql_flutter/graphql_flutter.dart' as graphQL;
+
+
 class Arcano {
 
   static const String SCHEME = 'http';
@@ -13,6 +18,30 @@ class Arcano {
   static const String EVENTS_URL = "$ARCANO/events";
   static const String AUTH_URL = "$ARCANO/auth";
   static const String USERS_URL_BY_USERNAME = USERS_URL + '/byUsername';
+
+  static const String GRAPH_QL_ENDPOINT = "$ARCANO/graphql";
+
+  static Link getGraphQlLink(){
+    return Link(getGraphQlUri().toString());
+  }
+
+  static Uri getGraphQlUri(){
+    return Uri(
+        scheme: SCHEME,
+        host: Arcano.IP_ADDRESS,
+        path: Arcano.GRAPH_QL_ENDPOINT,
+        port: Arcano.PORT
+    );
+  }
+
+  static Uri getEventsUri(){
+    return Uri(
+      scheme: SCHEME,
+      host: Arcano.IP_ADDRESS,
+      path: Arcano.EVENTS_URL,
+      port: Arcano.PORT
+    );
+  }
 
   static Uri getAuthUri(){
     return Uri(
@@ -40,4 +69,15 @@ class Arcano {
         port: Arcano.PORT
     );
   }
+}
+
+class ArcanoGraphQLLink extends graphQL.HttpLink{
+  ArcanoGraphQLLink() : super(Arcano.getGraphQlUri().toString());
+}
+
+class ArcanoGraphQLClient extends graphQL.GraphQLClient {
+  ArcanoGraphQLClient() : super(
+    link: ArcanoGraphQLLink(),
+    cache: graphQL.GraphQLCache(store: graphQL.InMemoryStore())
+  );
 }
