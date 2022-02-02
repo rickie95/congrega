57fb984e30e3a480314747e147e073cb50f67ccb
Riccardo Malavolti <riccardo.malavolti@gmail.com>
2022-02-02 11:47:37 +0100

    changes generic opponent's username

--- lib/features/lifecounter/data/PlayerRepository.dart
+++ lib/features/lifecounter/data/PlayerRepository.dart
@@ -4,16 +4,12 @@ import 'package:congrega/features/loginSignup/model/User.dart';
 import 'package:congrega/features/users/UserRepository.dart';
 
 class PlayerRepository {
-  
   PlayerRepository({required this.userRepository});
-  
+
   final UserRepository userRepository;
-  
-  Player fromUser(User user){
-    return Player(
-      user: user,
-      points: {new LifePoints(20)}
-    );
+
+  Player fromUser(User user) {
+    return Player(user: user, points: {new LifePoints(20)});
   }
 
   Future<Player> getAuthenticatedPlayer() async {
@@ -21,11 +17,7 @@ class PlayerRepository {
     return fromUser(user);
   }
 
-  Player genericOpponent(){
-    return fromUser(User(
-      id: 'generic-opponent',
-      name: 'Opponent',
-      username: 'generic-opponent'
-    ));
+  Player genericOpponent() {
+    return fromUser(User(id: 'generic-opponent', name: 'Opponent', username: 'Opponent'));
   }
 }
